dd0a98a71e75f49ce39cfd79cd71cc3182d1319b
steftones <stefansokolowski99@gmail.com>
2021-04-11 19:03:01 +0100

    style changes

--- src/components/PictureModal.js
+++ src/components/PictureModal.js
@@ -6,8 +6,6 @@ const PictureModal = ({ modalProject }) => {
 
   const pictures = modalProject.slice(1)
 
-  console.log(pictures)
-
   const extraPictures = pictures.map((carousel, index) => 
     <Carousel.Item key={index} interval={120000} >
       <img className="modalCarouselImage" src={carousel} alt={carousel.title}/>
@@ -19,7 +17,7 @@ const PictureModal = ({ modalProject }) => {
       <Carousel
       nextIcon={<div className="carouselButton">&nbsp;&nbsp;<i className="fa fa-chevron-right fa-3x" aria-hidden="true"/></div>}
       prevIcon={<div className="carouselButton"><i className="fa fa-chevron-left fa-3x" aria-hidden="true"/>&nbsp;&nbsp;</div>}
-      className="carouselWholezz"  slide={false}> 
+      slide={false}> 
         {extraPictures}
       </Carousel> 
     </div>
